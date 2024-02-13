adb0ede8692e0644e73a0438854d845745c72db4
Aryaroop Majumder <136429863+aryaroopmajumder@users.noreply.github.com>
2024-02-13 14:15:40 +0530

    fixed the logging statements

--- services/llama_model.py
+++ services/llama_model.py
@@ -9,17 +9,20 @@ import ollama
 
 load_dotenv()
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 class LlamaModel:
+
     def __init__(self):
+        model = os.getenv("OLLAMA_MODEL")
         try:
-            logging.debug("Entering LlamaModel")
-            model = os.getenv("OLLAMA_MODEL")
-            logging.debug(os.getenv("OLLAMA_MODEL"))
-            print(os.getenv("OLLAMA_MODEL"))
+            logger.debug("Entering LlamaModel")
+            logger.debug(f"Model selected is : {os.getenv('OLLAMA_MODEL')}")
             ollama.chat()
         except ollama.ResponseError as e:
-            print('Error:', e.error)
+            # print('Error:', e.error)
             if e.status_code == 404:
-                logging.error("Error as there is no model within")
-                # ollama.pull(model)
+                logger.error("Error as there is no model within")
+                ollama.pull(model)
