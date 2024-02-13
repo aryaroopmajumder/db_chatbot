513df3687d2ee56ff8b3642edc6a8e880623d4a4
Aryaroop Majumder <136429863+aryaroopmajumder@users.noreply.github.com>
2024-02-13 14:43:23 +0530

    added the install_requirements script file

--- services/llama_model.py
+++ services/llama_model.py
@@ -21,9 +21,7 @@ class LlamaModel:
             logger.debug("Entering LlamaModel")
             model_name = os.getenv("OLLAMA_MODEL")
             logger.debug(f"Model selected is : {model_name}")
-            ollama.chat(model=model_name)
+            ollama.pull('model_name')
         except ollama.ResponseError as e:
             # print('Error:', e.error)
-            if e.status_code == 404:
-                logger.error("Error as there is no model wait for the model to initialise")
-                ollama.pull(model)
+            logger.error("Model is not found")
